ef8b9d97d1da90f84a8470751515323823602cd5
Aibar Nurlanov <aibar.nurlanov@gmail.com>
2016-05-28 16:10:55 +0600

    wget install

---
+++ README.md
@@ -0,0 +1,1 @@
+    wget https://raw.githubusercontent.com/aibar/bash/master/install.sh | sh && source ~/.bash_profile
--- install.sh
+++ install.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
-cp .bash_profile ~
+cat > ~/.bash_profile << EOF 
+# Git
+function is_git() {
+    git rev-parse 2> /dev/null
+}
+function git_info() {
+    info="* $(git rev-parse --abbrev-ref HEAD)"
+    if [[ -n "$(git status --porcelain 2> /dev/null)" ]]; then
+        info="$info [no]"
+    fi
+    echo "$info"
+}
 
-echo 'Installed. run source ~/.bash_profile'
+# Colors
+Z="\033[m"
+Y="\033[1;33m"
+R="\033[1;31m"
+G="\033[1;32m"
+B="\033[1;34m"
+
+# Promt
+PS1="\[$Y\][\D{%T}] \w\[$Z\] \[$G\]\$(is_git && git_info)\[$Z\]\n\[$R\]\$ \[$Z\]"
+PROMPT_COMMAND='echo -ne "\033]0;${PWD/#$HOME/~}\007"'
+
+# Aliases
+alias la="ls -al $1"
+alias nn="nano"
+
+# Homes
+export JAVA_HOME=~/app/dev/jdk
+export JDK_HOME=~/app/dev/jdk
+export MAVEN_HOME=~/app/dev/maven
+export NODE_HOME=~/app/dev/node
+
+# Bins
+export PATH=$PATH:$HOME/bin:$MAVEN_HOME/bin:$NODE_HOME/bin:~/.npm_prefix/bin
+EOF
+
+exec bash
